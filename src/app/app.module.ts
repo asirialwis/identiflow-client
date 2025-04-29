ce8d627b3e2a1366c50831c61c9a986d1b7830d0
asirialwis <asirichathurika900@gmail.com>
2025-04-29 11:50:15 +0530

    initiate componets for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
